4293855c571b1d9a077ed4f688c71500e3247384
Eddy Barahona <ebarahona@premte.com>
2025-01-27 11:36:02 -0600

    [ADD] Maquetado del header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
+import Header from "@/home/header.component";
 import ProductsViewer from "@/home/products-viewer.component";
 import { productsService } from "@/services";
-
 
 export default async function Home() {
   const data = await productsService.getAllProducts();
@@ -9,36 +9,27 @@ export default async function Home() {
   return (
     <div className="w-full h-screen flex flex-col items-center">
       {/* Barra de navegación */}
-      <div className="w-full grid grid-cols-3">
-        {/* Logo */}
-        <div>
+      <Header />
+
+      <div className="w-full h-full flex flex-col overflow-y-auto">
+        {/* Banner principal */}
+        <div className="w-full">
 
         </div>
 
-        {/* Categorías */}
-        <div className="flex justify-center items-center">
+        <div className="w-full max-w-[1836px] flex flex-col gap-6">
+          {/* Categoría de recomendados */}
+          <ProductsViewer products={data} />
 
+          {/* Categoría de recomendados */}
+          <ProductsViewer products={data} />
+
+          {/* Categoría de recomendados */}
+          <ProductsViewer products={data} />
+
+          {/* Categoría de recomendados */}
+          <ProductsViewer products={data} />
         </div>
-
-        {/* Búsqueda y carrito*/}
-        <div className="flex justify-end items-center">
-
-        </div>
-      </div>
-
-      {/* Banner principal */}
-      <div className="w-full">
-
-      </div>
-
-      <div className="w-full max-w-[1836px] flex flex-col">
-        {/* Categoría de recomendados */}
-        <ProductsViewer products={data} />
-      </div>
-
-      {/* Categoría de Lo Más Nuevo */}
-      <div className="w-full">
-
       </div>
     </div>
   );
---
+++ src/home/header.component.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+export default function Header() {
+    return (
+        <div className="w-full grid grid-cols-3 p-4">
+            {/* Logo */}
+            <div>
+
+            </div>
+
+            {/* Categorías */}
+            <div className="flex justify-center items-center gap-4">
+                <p className="text-lg font-medium hover:underline hover:font-bold hover:text-xl cursor-pointer">Hombres</p>
+                <p className="text-lg font-medium hover:underline hover:font-bold hover:text-xl cursor-pointer">Mujeres</p>
+                <p className="text-lg font-medium hover:underline hover:font-bold hover:text-xl cursor-pointer">Joyería</p>
+                <p className="text-lg font-medium hover:underline hover:font-bold hover:text-xl cursor-pointer">Electrónicos</p>
+            </div>
+
+            {/* Búsqueda y carrito*/}
+            <div className="flex justify-end items-center gap-4">
+                <div className="bg-medium flex items-center pl-2 pr-20 py-1 gap-2 rounded-full cursor-pointer">
+                    <img src="svg/search.svg" alt="search" className="h-[25px] object-contain" />
+                    <p className="text-lg">Buscar</p>
+                </div>
+
+                <div className="flex items-center gap-2">
+                    <img src="svg/profile.svg" alt="profile" className="h-[30px] object-contain" />
+                    <img src="svg/cart.svg" alt="cart" className="h-[35px] object-contain" /> 
+                </div>
+            </div>
+        </div>
+    );
+}
